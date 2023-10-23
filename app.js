8a5801ef696550ffbb6b4fbdb8f3e32d7e2382bc
Ralph Greaves <rgreaves@ycp.edu>
2023-10-23 17:42:39 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const { auth } = require('express-openid-connect');
+
+const app = express();
+
+const port = process.env.PORT;
+
+app.use(
+  auth({
+    authorizationParams: {
+      response_type: 'code',
+      audience: process.env.AUDIENCE,
+      scope: 'openid profile email'
+    }
+  })
+);
+
+app.get('/', (req, res) => {
+  res.status(200).send('OK');
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
